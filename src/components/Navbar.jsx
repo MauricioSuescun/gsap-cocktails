56119c094311562b7ca99231541b0dd7001a1bb3
MauricioSuescun <110485721+MauricioSuescun@users.noreply.github.com>
2025-06-28 13:06:18 -0500

    navbar section

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,50 @@
+import { useGSAP } from "@gsap/react";
+import { navLinks } from "../../constants";
+import gsap from "gsap";
+
+const Navbar = () => {
+  useGSAP(() => {
+    const navTween = gsap.timeline({
+      scrollTrigger: {
+        trigger: "nav",
+        start: "top top",
+        end: "bottom top",
+        scrub: true,
+        markers: true,
+      },
+    });
+    navTween.fromTo(
+      "nav",
+      { backgroundColor: "transparent" },
+      {
+        backgroundColor: "#00000050",
+        backgroundFilter: "blur(10px)",
+        duration: 1,
+        ease: "power1.inOut",
+      }
+    );
+  });
+
+  return (
+    <nav className="p-4">
+      <div className="container mx-auto">
+        <a href="#home" className="flex items-center gap-2">
+          <img src="/images/logo.png" alt="Velvet Pour Logo" />
+          <p>Velvet Pour</p>
+        </a>
+
+        <ul>
+          {navLinks.map((link) => (
+            <li key={link.id}>
+              <a href={`#${link.id}`} className="text-white">
+                {link.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
