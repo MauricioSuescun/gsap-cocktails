e9b8e936fb579859efe69f784ca982e0b60fe519
MauricioSuescun <110485721+MauricioSuescun@users.noreply.github.com>
2025-07-03 00:31:11 -0500

    about section completed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -75,7 +75,7 @@ const Hero = () => {
   return (
     <>
       <section id="hero" className="noisy">
-        <h1 className="title">COCKTAILS</h1>
+        <h1 className="title">DRINKS</h1>
 
         <img
           src="/images/hero-left-leaf.png"
